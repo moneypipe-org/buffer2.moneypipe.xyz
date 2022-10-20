9e52b8e1f8b4a57b56c74c62bb1ad8821ce0f000
skøgard <skogard@protonmail.com>
2022-10-20 13:52:29 -0400

    Polygon

--- mumbai/constants.js
+++ mumbai/constants.js
@@ -1,5 +1,6 @@
 const Constants = {
   weth: "0xa6fa4fb5f76172d178d61b04b0ecd319c5d1c0aa",
   net: "mumbai",
-  chainId: 80001
+  chainId: 80001,
+  currency: "MATIC",
 }
---
+++ polygon/constants.js
@@ -0,0 +1,6 @@
+const Constants = {
+  weth: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
+  net: "polygon",
+  chainId: 137,
+  currency: "MATIC",
+}
